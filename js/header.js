820c91cb2044c4b722cb97c06e200d5ef3abbdae
kyohyunkim <86403089+kyohyunkim@users.noreply.github.com>
2022-03-10 15:06:49 +0900

    v-1

--- js/header.js
+++
@@ -1,7 +0,0 @@
-cross_img.addEventListener('click',function(){
-    cross_img.classList.toggle('active');
-    big_ul.classList.toggle('active');
-});
-shop_btn.addEventListener('click',function(){
-    small_ul.classList.toggle('active');
-});
--- js/inc.js
+++
@@ -1,8 +0,0 @@
-function letsGo(){
-
-    fetch('/data/data.json')
-    .then( res => res.json() )
-    .then( data => callback(data));
-
-};
-window.onload = letsGo;
