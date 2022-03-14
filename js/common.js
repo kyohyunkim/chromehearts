5d7acf639c7d6d2b80ad22ee0957384f58cfca1b
kyohyunkim <86403089+kyohyunkim@users.noreply.github.com>
2022-03-14 17:27:34 +0900

    v-1

--- js/common.js
+++ js/common.js
@@ -1,6 +1,4 @@
 $('header').load('inc.html header > div', function(){
-    const mainchange = document.querySelector('body');
-
     cross_img.addEventListener('click',function(){
         cross_img.classList.toggle('active');
         big_ul.classList.toggle('active');
